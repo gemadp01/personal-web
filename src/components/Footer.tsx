4b6c0f16a0b2d5b2c0cde05122c3e720911d210a
Gema Dodi Pranata <gemadp01@gmail.com>
2025-05-27 14:57:06 +0700

    feat: footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="min-h-24 flex justify-center items-center">
+      <p className="text-sm text-seventh">
+        Copyright © 2025 Gema Dodi Pranata.
+      </p>
+    </footer>
+  );
+};
