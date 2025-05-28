70fa70a82e887faefa4ffd40e2a0cf9d58b017df
Gema Dodi Pranata <gemadp01@gmail.com>
2025-05-28 12:26:54 +0700

    feat: konfigurasi default style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" Component={AboutPage}></Route>
+      </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,20 @@
+const AboutPage = () => {
+  return (
+    <main className="min-h-[80vh] flex items-center text-center">
+      <div className="space-y-14 px-10">
+        <div className="space-y-3">
+          <h4>Hi, I’m Test,</h4>
+          <h4>a Frontend Engineer</h4>
+        </div>
+        <p className="text-seventh">
+          Hi! I’m a frontend developer who enjoys crafting responsive and
+          user-friendly web apps. With a strong base in JavaScript, React, and
+          Laravel, I love working in agile teams and taking on meaningful
+          challenges. Let’s build something awesome together!
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default AboutPage;
