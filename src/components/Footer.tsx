9b73cc9bcf904973c0018b80b1cb54cfa1a3fdef
Gema Dodi Pranata <gemadp01@gmail.com>
2025-05-28 12:25:17 +0700

    fix(footer): tidak selaras

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="min-h-24 flex justify-center items-center">
+    <footer className="min-h-24 flex justify-center items-center fixed bottom-0 right-0 left-0">
       <p className="text-sm text-seventh">
         Copyright © 2025 Gema Dodi Pranata.
       </p>
