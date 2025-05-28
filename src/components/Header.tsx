f43ffea7ea9c7433e7faa3ac49f5f7f9a570ee05
Gema Dodi Pranata <gemadp01@gmail.com>
2025-05-28 12:26:28 +0700

    feat(header): responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,42 @@
+import { useEffect, useState } from "react";
 import { DropdownMenu } from "./ui/DropdownMenu";
 
 export const Header = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(false);
+
+  const navItems = ["About", "Projects", "Contact"];
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth >= 640);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
-    <header className="flex justify-around items-center h-24">
-      <h2 className="bg-color-dark">Test</h2>
+    <header className="min-w-full px-6 flex justify-between items-center h-24">
+      <h2 className="text-primary">Test</h2>
 
       <nav>
-        <DropdownMenu items={["About", "Projects", "Contact"]} />
+        {isSmallScreen ? (
+          <ul className="flex gap-6">
+            {navItems.map((item) => (
+              <li key={item}>
+                <a
+                  href={`${item.toLowerCase()}`}
+                  className="text-primary hover:text-seventh"
+                >
+                  {item}
+                </a>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <DropdownMenu items={navItems} />
+        )}
       </nav>
     </header>
   );
