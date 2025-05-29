6075d7f4e9521e33fbded9d38a6c4ad7c65e1854
Gema Dodi Pranata <gemadp01@gmail.com>
2025-05-29 19:15:17 +0700

    rafactoring(AboutPage)

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,19 +1,21 @@
+import { DownloadIcon } from "lucide-react";
+import { Button } from "../components/ui/Button";
 import { useIsSmallScreen } from "../lib/utils";
 
 const AboutPage = () => {
-  const isSmallScreen = useIsSmallScreen(640);
+  const isSmallScreen: boolean = useIsSmallScreen(640);
 
   return (
     <main className="min-h-[80vh] flex items-center text-center">
       <div className="space-y-10 md:space-y-14 px-10">
         {isSmallScreen ? (
           <h2 className="space-y-5">
-            <span className="block">Hi, I’m Test,</span>
+            <span className="block">Hi, I’m Gema Dodi Pranata,</span>
             <span className="block">a Frontend Engineer</span>
           </h2>
         ) : (
           <h4 className="space-y-3">
-            <span className="block">Hi, I’m Test,</span>
+            <span className="block">Hi, I’m Gema Dodi Pranata,</span>
             <span className="block">a Frontend Engineer</span>
           </h4>
         )}
@@ -23,6 +25,15 @@ const AboutPage = () => {
           Laravel, I love working in agile teams and taking on meaningful
           challenges. Let’s build something awesome together!
         </p>
+        <Button
+          href="https://drive.google.com/file/d/13IpPGjuM2PqLxS2BbQ5uVd1RtbN1gQRp/view?usp=sharing"
+          icon={<DownloadIcon className="mr-2 w-4 h-4" />}
+          iconPosition="left"
+          color="secondary"
+          external={true}
+        >
+          Download CV
+        </Button>
       </div>
     </main>
   );
