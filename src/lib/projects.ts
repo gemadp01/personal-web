9242655c17a88423865f7a16608606ac1e052897
Gema Dodi Pranata <gemadp01@gmail.com>
2025-05-29 14:08:00 +0700

    feat: data dummy

---
+++ src/lib/projects.ts
@@ -0,0 +1,68 @@
+export interface Project {
+  image: string;
+  subtitle: string;
+  title: string;
+  description: string;
+  link: string;
+}
+
+export const dataDummy: Project[] = [
+  {
+    image: "/your-image-path.jpg",
+    subtitle: "Fullstack",
+    title: "My Tayu Web App",
+    description: "Laravel",
+    link: "https://github.com/gemadp01/mytayu-app",
+  },
+
+  {
+    image: "/your-image-path.jpg",
+    subtitle: "Fullstack",
+    title: "Simple E-Commerce",
+    description: "Simple Project E-Commerce with Laravel",
+    link: "https://github.com/gemadp01/151-gema-dodi-pranata",
+  },
+  {
+    image: "/your-image-path.jpg",
+    subtitle: "Frontend",
+    title: "FastCampus Commerce",
+    description: "React Redux",
+    link: "https://github.com/gemadp01/react-redux",
+  },
+  {
+    image: "/your-image-path.jpg",
+    subtitle: "Frontend",
+    title: "Apple ID Landing Page",
+    description: "Clone Apple ID with HTML CSS Javascript",
+    link: "https://github.com/gemadp01/apple-id",
+  },
+  {
+    image: "/your-image-path.jpg",
+    subtitle: "Frontend",
+    title: "Agency Landing Page",
+    description: "Design to Code Agency Landing Page with HTML CSS Javascript",
+    link: "https://github.com/gemadp01/agency-landing-page",
+  },
+
+  {
+    image: "/your-image-path.jpg",
+    subtitle: "Backend",
+    title: "Simple Todolist",
+    description: "Simple Project Todolist with php",
+    link: "https://github.com/gemadp01/usjwp-todolist",
+  },
+  {
+    image: "/your-image-path.jpg",
+    subtitle: "UI UX",
+    title: "My Tayu Design System",
+    description: "My Tayu Design System with Figma",
+    link: "https://github.com/gemadp01/usjwp-todolist",
+  },
+  {
+    image: "/your-image-path.jpg",
+    subtitle: "UI UX",
+    title: "Personal Web Design System",
+    description: "Personal Web Design System with Figma",
+    link: "https://github.com/gemadp01/usjwp-todolist",
+  },
+];
