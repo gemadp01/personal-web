b74da3effb2ccc5dc26720d0cf524a1c466885cf
Gema Dodi Pranata <gemadp01@gmail.com>
2025-05-29 21:10:21 +0700

    fix(DropdownMenu): component content belum terbungkus dengan portal

--- src/components/ui/DropdownMenu.tsx
+++ src/components/ui/DropdownMenu.tsx
@@ -9,32 +9,37 @@ export const DropdownMenu = ({ ...props }: { items: string[] }) => {
 
   return (
     <DropdownMenuPrimitive.Root>
-      <DropdownMenuPrimitive.Trigger className="relative" asChild>
+      <DropdownMenuPrimitive.Trigger asChild>
         <button>
           <Menu className="w-6 h-6" />
         </button>
       </DropdownMenuPrimitive.Trigger>
-      <DropdownMenuPrimitive.Content className="border rounded" align="end">
-        {items.map((item: string) => {
-          const path = `/${item.toLowerCase()}`;
-          const isActive = currentPath === path;
-          return (
-            <DropdownMenuPrimitive.Item asChild className="flex">
-              <Link
-                key={item}
-                to={path}
-                className={`flex px-10 py-3 ${
-                  isActive
-                    ? "text-light pointer-events-none bg-dark"
-                    : "text-primary hover:text-light hover:bg-dark"
-                }`}
-              >
-                {item}
-              </Link>
-            </DropdownMenuPrimitive.Item>
-          );
-        })}
-      </DropdownMenuPrimitive.Content>
+      <DropdownMenuPrimitive.Portal>
+        <DropdownMenuPrimitive.Content
+          className="border border-dark rounded bg-secondary"
+          align="end"
+        >
+          {items.map((item: string) => {
+            const path = `/${item.toLowerCase()}`;
+            const isActive = currentPath === path;
+            return (
+              <DropdownMenuPrimitive.Item asChild className="flex">
+                <Link
+                  key={item}
+                  to={path}
+                  className={`flex px-10 py-3 ${
+                    isActive
+                      ? "text-light pointer-events-none bg-dark"
+                      : "text-primary hover:text-light hover:bg-dark"
+                  }`}
+                >
+                  {item}
+                </Link>
+              </DropdownMenuPrimitive.Item>
+            );
+          })}
+        </DropdownMenuPrimitive.Content>
+      </DropdownMenuPrimitive.Portal>
     </DropdownMenuPrimitive.Root>
   );
 };
