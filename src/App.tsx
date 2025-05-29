478ae8608c006c43b680fc7e6869ff3858deae52
Gema Dodi Pranata <gemadp01@gmail.com>
2025-05-29 19:19:47 +0700

    feat(ContactPage): set route notfoundpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import ProjectsPage from "./pages/ProjectsPage";
 import ContactPage from "./pages/ContactPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" Component={AboutPage}></Route>
-        <Route path="/projects" Component={ProjectsPage}></Route>
-        <Route path="/contact" Component={ContactPage}></Route>
+        <Route path="/" element={<Navigate to="/about" />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" Component={NotFoundPage} />
       </Routes>
       <Footer />
     </>
