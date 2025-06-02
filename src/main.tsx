a2350bf50bf0133e3aa7a069b37588454fbc34a9
Gema Dodi Pranata <gemadp01@gmail.com>
2025-06-02 19:05:28 +0700

    feat/dark-mode: set provider pada main

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import DarkModeContextProvider from "./context/DarkMode.tsx";
+import { ThemeProvider } from "./context/ThemeProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <DarkModeContextProvider>
+    <ThemeProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </DarkModeContextProvider>
+    </ThemeProvider>
   </StrictMode>
 );
