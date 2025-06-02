fa059aa9efacdf36301dc73ef9576febcc82d5d0
Gema Dodi Pranata <gemadp01@gmail.com>
2025-06-02 19:29:04 +0700

    feat/dark-mode: project card

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -32,9 +32,11 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
     <div className="rounded-xl overflow-hidden shadow-xl bg-white w-80">
       <img src={image} alt={title} className="w-full h-40 object-cover" />
       <div className="p-4 space-y-2">
-        <p className="text-sm text-extended">{subtitle}</p>
-        <h2 className="text-lg font-semibold text-primary">{title}</h2>
-        <p className="text-sm text-seventh">{description}</p>
+        <p className="text-sm text-extended ">{subtitle}</p>
+        <h2 className="text-lg font-semibold text-primary dark:text-secondary">
+          {title}
+        </h2>
+        <p className="text-sm text-seventh dark:text-sixth">{description}</p>
 
         <Button
           link={isInternal ? link : undefined}
