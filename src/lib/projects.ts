84d15a7082b0beb299d16764d6135b3f77af35d9
Gema Dodi Pranata <gemadp01@gmail.com>
2025-06-25 22:18:32 +0700

    feat: data projects

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,6 +1,6 @@
 export interface Project {
   image: string;
-  subtitle: string;
+  category: string;
   title: string;
   description: string;
   link: string;
@@ -9,58 +9,68 @@ export interface Project {
 export const dataDummy: Project[] = [
   {
     image: "/your-image-path.jpg",
-    subtitle: "Fullstack",
+    category: "Fullstack",
     title: "My Tayu Web App",
-    description: "Laravel",
+    description:
+      "Developed a web application to manage final project (TA) activities, including registration validation, data import, supervisor assignment, and graduation (yudisium) information display, using Laravel 8.",
     link: "https://github.com/gemadp01/mytayu-app",
   },
 
   {
     image: "/your-image-path.jpg",
-    subtitle: "Fullstack",
+    category: "Fullstack",
     title: "Simple E-Commerce",
-    description: "Simple Project E-Commerce with Laravel",
-    link: "https://github.com/gemadp01/151-gema-dodi-pranata",
+    description: "Simple Project E-Commerce with Laravel 9 and Bootstrap 5.",
+    link: "https://github.com/gemadp01/laravel9-simple-ecommerce",
   },
   {
     image: "/your-image-path.jpg",
-    subtitle: "Frontend",
+    category: "Frontend",
     title: "FastCampus Commerce",
-    description: "React Redux",
+    description:
+      "Built a real-case project featuring product search, shopping cart, checkout, and admin page using React and Redux.",
     link: "https://github.com/gemadp01/react-redux",
   },
   {
     image: "/your-image-path.jpg",
-    subtitle: "Frontend",
-    title: "Apple ID Landing Page",
-    description: "Clone Apple ID with HTML CSS Javascript",
-    link: "https://github.com/gemadp01/apple-id",
+    category: "Frontend",
+    title: "Simple todoapp",
+    description:
+      "Built a simple todo app with CRUD and search features using json-server, styled with Tailwind CSS and DaisyUI, developed in Nextjs 13.",
+    link: "https://github.com/gemadp01/simple-todoapp",
   },
   {
     image: "/your-image-path.jpg",
-    subtitle: "Frontend",
-    title: "Agency Landing Page",
-    description: "Design to Code Agency Landing Page with HTML CSS Javascript",
-    link: "https://github.com/gemadp01/agency-landing-page",
+    category: "Frontend",
+    title: "Apple ID Landing Page",
+    description: "Clone Apple ID with HTML, CSS, Javascript.",
+    link: "https://github.com/gemadp01/apple-id",
   },
+  // {
+  //   image: "/your-image-path.jpg",
+  //   subtitle: "Frontend",
+  //   title: "Agency Landing Page",
+  //   description: "Design to Code Agency Landing Page with HTML CSS Javascript",
+  //   link: "https://github.com/gemadp01/agency-landing-page",
+  // },
 
   {
     image: "/your-image-path.jpg",
-    subtitle: "Backend",
+    category: "Backend",
     title: "Simple Todolist",
     description: "Simple Project Todolist with php",
     link: "https://github.com/gemadp01/usjwp-todolist",
   },
   {
     image: "/your-image-path.jpg",
-    subtitle: "UI UX",
+    category: "UI UX",
     title: "My Tayu Design System",
     description: "My Tayu Design System with Figma",
     link: "https://www.figma.com/design/EdA9EdmO8bLSiVubG9dLmS/Wireframe-TAYU?node-id=0-1&t=ltXPbBK2adhUqTmH-1",
   },
   {
     image: "/your-image-path.jpg",
-    subtitle: "UI UX",
+    category: "UI UX",
     title: "Personal Web Design System",
     description: "Personal Web Design System with Figma",
     link: "https://www.figma.com/design/49wpoz7gtVv3PvbLH8yowo/Personal-Website?node-id=234-61&t=VASS8SKuOqw0906o-1",
