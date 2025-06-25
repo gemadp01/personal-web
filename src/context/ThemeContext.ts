05c772b72117798fa63c65591dbd811727529e63
Gema Dodi Pranata <gemadp01@gmail.com>
2025-06-25 23:14:51 +0700

    feat: adding a type to the context variable inside the theme context

--- src/context/ThemeContext.ts
+++ src/context/ThemeContext.ts
@@ -12,7 +12,7 @@ export const ThemeContext = createContext<ThemeContextType | undefined>(
 );
 
 export const useTheme = () => {
-  const context = useContext(ThemeContext);
+  const context = useContext<ThemeContextType | undefined>(ThemeContext);
   if (context === undefined) {
     throw new Error("useTheme must be used within a ThemeProvider");
   }
